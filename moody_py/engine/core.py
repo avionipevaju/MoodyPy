abdd42ad591239389b5a7e3889c2d6a724498dc6
avionipevaju <antic.nikola@outlook.com>
2018-08-25 16:44:16 +0200

    Implemented engine core. Added scheduler.

---
+++ moody_py/engine/core.py
@@ -0,0 +1,46 @@
+from moody_py.engine.moody import Moody
+from moody_py.forecast.forecast import Forecast
+from moody_py.youtube.youtube import YouTube
+from moody_py.discogs.discogs import Discogs
+from moody_py.storage.storage import Redis
+import schedule
+import time
+
+
+class Core:
+
+    def __init__(self):
+        self.moody = Moody()
+        self.moody.verify_credentials()
+        self.weather = Forecast('Belgrade')
+        self.youtube_search_engine = YouTube()
+        self.discogs = Discogs()
+        self.redis_engine = Redis()
+
+    def execute_task(self):
+        artist = 'Chroma Key'
+        track = self.discogs.get_random_track_by_artist(artist)
+        print(track)
+
+        track_by_genre = self.discogs.get_random_track_by_genre('Post+Rock')
+        print(track_by_genre)
+
+        print(self.redis_engine.get_string('app:name'))
+
+        weather_data = self.weather.current_weather()
+
+        self.moody.tweet(weather_data.location + ' ' + weather_data.condition + ' ' +
+                         self.youtube_search_engine.search_video(track_by_genre))
+
+    def schedule(self):
+        schedule.every(30).seconds.do(self.execute_task)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    core = Core()
+    core.schedule()
+
+
